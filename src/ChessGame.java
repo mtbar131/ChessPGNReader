9e5f1f0eff3e91ab67bb63d280c176687c9f7a03
Anubhav Chaturvedi <mailforanubhav@gmail.com>
2015-07-29 13:43:28 +0530

    implemented the constructor

--- src/ChessGame.java
+++ src/ChessGame.java
@@ -1,8 +1,12 @@
 
 public class ChessGame {
 
+	private ChessBoard board;
+	private MoveParser moveParser;
+	
 	ChessGame() {
-		
+		this.board = new ChessBoard();
+		this.moveParser = new MoveParser();
 	}
 	
 	void processPGN(ChessBoard initialBoardState, String PGN_filepath) {
